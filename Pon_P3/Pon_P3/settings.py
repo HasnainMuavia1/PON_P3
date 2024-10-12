249de80d4daa8a63f1c690ea2a95fafc7ed67213
Hasnain Muavia <131387615+HasnainMuavia1@users.noreply.github.com>
2024-10-12 20:59:39 +0500

    Update settings.py

--- Pon_P3/Pon_P3/settings.py
+++ Pon_P3/Pon_P3/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hasnain.muavia2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'ezoh mcaq jxnn pkws'
+EMAIL_HOST_USER = '<Email>'
+EMAIL_HOST_PASSWORD =  '<Password>'
